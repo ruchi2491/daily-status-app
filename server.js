25465df2561580f5d752e38be66c53162473c294
ATMECS\ruchira.more <ruchira.more@atmecs.com>
2018-07-26 11:13:41 +0530

    Changed package file

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('*', (req, res) => {
 });
 
 //Set Port
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3001';
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
